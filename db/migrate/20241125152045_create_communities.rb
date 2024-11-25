cc69eece770ca94b801ec3999747fcfb3a4f780e
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-25 16:23:46 +0100

    add routes, controllers, and models

---
+++ db/migrate/20241125152045_create_communities.rb
@@ -0,0 +1,13 @@
+class CreateCommunities < ActiveRecord::Migration[7.1]
+  def change
+    create_table :communities do |t|
+      t.string :name
+      t.text :description
+      t.string :category
+      t.references :book, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
