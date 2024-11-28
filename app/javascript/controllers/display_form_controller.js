6846734d37a293cd2845198e219d38faa18ddf1f
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-28 21:24:22 +0100

    add pop-up modal to the questionaire & fix minor styling issues

--- app/javascript/controllers/display_form_controller.js
+++ app/javascript/controllers/display_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="display-form"
 export default class extends Controller {
-  static targets = ["questionaire", "question_1", "question_2", "question_3", "question_4"]; // Corrected typo and syntax
+  static targets = ["modal", "questionaire", "question_1", "question_2", "question_3", "question_4"];
 
   connect() {
     console.log("Hi from display_form");
@@ -11,6 +11,12 @@ export default class extends Controller {
   render() {
     console.log("Clicked!");
     this.questionaireTarget.classList.remove("d-none")
+    this.modalTarget.classList.remove("hidden");
+    document.querySelector(".custom-container").classList.add("modal-visible");
+  }
+
+  close() {
+    this.modalTarget.classList.add("hidden");
+    document.querySelector(".custom-container").classList.remove("modal-visible");
   }
 }
-
