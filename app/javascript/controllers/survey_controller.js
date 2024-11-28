c27e6cc088f2c9689e7ad6cf8f05f53da6f93710
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-28 12:06:25 +0100

    js added to move the questions on the questionaire

--- app/javascript/controllers/survey_controller.js
+++ app/javascript/controllers/survey_controller.js
@@ -2,30 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="survey"
 export default class extends Controller {
-  static targets = ["answer", "input", "survey"]
+  static targets = ["answer", "input", "survey", "question_1", "question_2", "question_3", "question_4", "question_5"]
 
   connect() {
+    this.currentQuestionIndex = 0; // Start from the first question
+    this.questionTargets = [this.question_1Target, this.question_2Target, this.question_3Target, this.question_4Target, this.question_5Target];
   }
 
-  createAnswer(event){
-    event.preventDefault()
-    console.log(event.currentTarget.dataset.answer)
+  createAnswer(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.dataset.answer);
 
-    this.inputTarget.value = event.currentTarget.dataset.answer
+    this.inputTarget.value = event.currentTarget.dataset.answer;
 
     fetch(this.surveyTarget.action, {
       method: "POST",
-      // headers: {"Content-Type": "application/json"},
       body: new FormData(this.surveyTarget)
     })
     .then(response => response.json())
     .then((data) => {
       console.log(data);
-    })
+      this.showNextQuestion();
+    });
+  }
 
+  showNextQuestion() {
+    this.questionTargets[this.currentQuestionIndex].classList.add("d-none");
 
-    // proximos passos:
-    // 1. colocar classe d-none do formulario atual
-    // 2. tirar classe d-none do proximo formulario 
+    this.currentQuestionIndex += 1;
+
+    if (this.currentQuestionIndex < this.questionTargets.length) {
+      this.questionTargets[this.currentQuestionIndex].classList.remove("d-none");
+    } else {
+      console.log("Survey completed. Generate recommendation");
+    }
   }
 }
