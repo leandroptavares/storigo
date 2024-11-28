1891f07232325025565213c64aa979a0ac2eca73
Yupeng Wang <yupengori@gmail.com>
2024-11-28 21:16:15 +0100

    search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,19 +8,19 @@ class BooksController < ApplicationController
     # Search by title
     # url = "https://api2.isbndb.com/books/#{user_input}?page=1&pageSize=20&column=title&language=en&shouldMatchAll=0"
     #Search by category
-    # url = "https://api2.isbndb.com/subject/#{user_input}"
-    #Search by author
-    url = "https://api2.isbndb.com/author/#{user_input}?page=1&pageSize=20"
+    url = "https://api2.isbndb.com/subject/#{user_input}"
+    # url = "https://api2.isbndb.com/search/#{user_input}?page=1&pageSize=20"  NOT GOOD
+    # Search by author
+    # url = "https://api2.isbndb.com/author/#{user_input}?page=1&pageSize=20"
 
       response = HTTP.headers("Content-Type": "application/json", "Authorization": ENV["ISBN_DB_API"]).get(url)
       books = JSON.parse(response.body)
       @books_ai = books["books"]
       @books_ai.each do |book_data|
         create(book_data)
-
       end
     else
-      @books_ai = Book.all.sample(10)
+      @books_ai = Book.all
     end
   end
 
