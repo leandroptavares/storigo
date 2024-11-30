5944279d7d9b66bf09b2a4500f6346f34aab144b
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-30 23:34:20 +0100

    adjust user_books

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,6 +1,6 @@
 class UserBooksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_book, only: [:destroy]
+  before_action :set_book, only: [:destroy, :edit, :update]
 
   def index
     @user_books = current_user.books
@@ -11,21 +11,38 @@ class UserBooksController < ApplicationController
     @user_book = UserBook.new
     @user_book.user_id = current_user.id
     @user_book.book_id = @book.id
-    @user_book.save
+    @user_book.status = "In progress"
+    @user_book.pages_read = 0
 
-    # if @user_book.save
-    #   redirect_to user_books_path, notice: 'Book added to your bookshelf.'
-    # else
-    #   redirect_to book_path(@book), alert: 'Unable to add the book to your bookshelf.'
-    # end
+    if @user_book.save
+      redirect_to user_books_path, notice: 'Book added to your bookshelf.'
+    else
+      redirect_to book_path(@book), alert: 'Unable to add the book to your bookshelf.'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user_book.update(user_book_params)
+    if @user_book.save
+      redirect_to user_books_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @book.destroy
-    redirect_to user_books_path, status: :see_other
+    @user_book.destroy
+    redirect_to user_books_path, status: :see_other, notice: "Book removed from your bookshelf."
   end
 
   private
+
+  def user_book_params
+    params.require(:user_book).permit(:status)
+  end
 
   def set_book
     @user_book = UserBook.find(params[:id])
