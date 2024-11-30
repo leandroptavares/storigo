28131cd701c9199114eb11bc0e25bac01253176e
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-30 21:45:10 +0100

    fix create user_books

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,21 +1,33 @@
 class UserBooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_book, only: [:destroy]
 
   def index
     @user_books = current_user.books
   end
 
   def create
-    @book = Book.find(params[:book_id])
+    @book = Book.find(params[:id])
     @user_book = UserBook.new
     @user_book.user_id = current_user.id
     @user_book.book_id = @book.id
     @user_book.save
 
-    if @user_book.save
-      redirect_to user_books_path, notice: 'Book added to your bookshelf.'
-    else
-      redirect_to book_path(@book), alert: 'Unable to add the book to your bookshelf.'
-    end
+    # if @user_book.save
+    #   redirect_to user_books_path, notice: 'Book added to your bookshelf.'
+    # else
+    #   redirect_to book_path(@book), alert: 'Unable to add the book to your bookshelf.'
+    # end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to user_books_path, status: :see_other
+  end
+
+  private
+
+  def set_book
+    @user_book = UserBook.find(params[:id])
   end
 end
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,23 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="modal"
-export default class extends Controller {
-  static targets = ["modal", "button"];
+// // Connects to data-controller="modal"
+// export default class extends Controller {
+//   static targets = ["modal", "button"];
 
-  connect() {
-    // console.log("modal is here")
-  }
+//   connect() {
+//     // console.log("modal is here")
+//   }
 
-  show() {
-    console.log("clicked")
-    this.modalTarget.classList.remove("hidden");
-    this.buttonTarget.innerText = "Added to Bookshelf";
-    this.buttonTarget.disabled = true;
-    document.querySelector(".book-container").classList.add("modal-visible");
-  }
+//   show() {
+//     console.log("clicked")
+//     this.modalTarget.classList.remove("hidden");
+//     this.buttonTarget.innerText = "Added to Bookshelf";
+//     this.buttonTarget.disabled = true;
+//     document.querySelector(".book-container").classList.add("modal-visible");
+//   }
 
-  close() {
-    this.modalTarget.classList.add("hidden");
-    document.querySelector(".book-container").classList.remove("modal-visible");
-  }
-}
+//   close() {
+//     this.modalTarget.classList.add("hidden");
+//     document.querySelector(".book-container").classList.remove("modal-visible");
+//   }
+// }
