6b870e4566c67bb252c4d2869e845b5ab3f24242
Yupeng Wang <yupengori@gmail.com>
2024-12-03 18:01:36 +0100

    comments in community

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,16 @@
 class MessagesController < ApplicationController
-
   def create
     @community = Community.find(params[:community_id])
     @message = Message.new(message_params)
     @message.community = @community
     @message.user = current_user
+
     if @message.save
       respond_to do |format|
         format.turbo_stream do
           render turbo_stream: turbo_stream.prepend(:messages, partial: "messages/message",
             target: "messages",
-            locals: { message: @message })
+            locals: { message: @message, current_user: current_user })
         end
         format.html { redirect_to booking_path(@community) }
       end
