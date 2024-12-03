8d8ca65b659eb84eda7f0f6f3632808fe02fc199
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-12-03 17:46:11 +0100

    add recent recommendations page

--- app/controllers/user_reactions_controller.rb
+++ app/controllers/user_reactions_controller.rb
@@ -1,2 +1,20 @@
 class UserReactionsController < ApplicationController
+
+  def index
+    @user_reactions = current_user.user_reactions
+  end
+
+  def create
+    @user_reaction = UserReaction.new(user_reaction_params)
+    @user_reaction.user_id = current_user.id
+    if @user_reaction.save
+      render json: { message: "Created", user_reaction: @user_reaction }, status: :created
+    end
+  end
+
+  private
+
+  def user_reaction_params
+    params.require(:user_reaction).permit(:like, :book_id)
+  end
 end
