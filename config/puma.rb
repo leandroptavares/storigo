90141bdd74dec4fc4e197c9f15ae6b5df59a409a
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-12-04 16:56:41 +0100

    proc

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
+plugin :solid_queue
 
-if Rails.env.development?
-  plugin :solid_queue
-end
